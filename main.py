010cca4295a99f459ce9148bddb03eee2ec53ae2
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-05-26 11:43:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
